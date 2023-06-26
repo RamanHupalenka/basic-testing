9ead54d5b74af19eccc9e140ecedf44506e55552
Raman Hupalenka <gupalenko.98@mail.ru>
2023-06-26 20:53:58 +0300

    feat: implement table tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,51 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
-const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+interface TestCasePayload {
+  a: number | unknown;
+  b: number | unknown;
+  action: Action | unknown;
+  expected: number | null;
+}
+
+type TestCaseWithDescription = [string, TestCasePayload];
+
+const testCases: TestCaseWithDescription[] = [
+  ['should add two numbers', { a: 1, b: 2, action: Action.Add, expected: 3 }],
+  [
+    'should substract two numbers',
+    { a: 3, b: 2, action: Action.Substract, expected: 1 },
+  ],
+  [
+    'should multiply two numbers',
+    { a: 3, b: 3, action: Action.Multiply, expected: 9 },
+  ],
+  [
+    'should divide two numbers',
+    { a: 9, b: 3, action: Action.Divide, expected: 3 },
+  ],
+  [
+    'should exponentiate two numbers',
+    { a: 4, b: 2, action: Action.Exponentiate, expected: 16 },
+  ],
+  [
+    'should return null for invalid action',
+    { a: 1, b: 2, action: 'sum', expected: null },
+  ],
+  [
+    'should return null for invalid arguments',
+    { a: '2', b: '1', action: Action.Add, expected: null },
+  ],
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('%s', (_, { a, b, action, expected }) => {
+    const result = simpleCalculator({
+      a,
+      b,
+      action,
+    });
+
+    expect(result).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
